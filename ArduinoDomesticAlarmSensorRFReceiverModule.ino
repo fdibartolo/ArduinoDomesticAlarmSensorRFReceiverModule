1c04cad4a270c948e2e9f9575c89c11a113d1484
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-01-19 15:09:14 -0300

    add check for wired magnetic sensor

--- ArduinoDomesticAlarmSensorRFReceiverModule.ino
+++ ArduinoDomesticAlarmSensorRFReceiverModule.ino
@@ -1,11 +1,17 @@
 #include <VirtualWire.h>
 
 const int pinOutputLed = 13;
+const int pinOutputSensorTriggered = 2;
+const int pinInputSensor1 = 3;
+
 const char expectedMessage[5] = {'b','o','o','m'};
 char actualMessage[5];
 
 void setup() {                
   pinMode(pinOutputLed, OUTPUT);     
+  pinMode(pinOutputSensorTriggered, OUTPUT);
+  pinMode(pinInputSensor1, INPUT);
+
   vw_setup(2000); //Bits per sec
   vw_rx_start();
   
@@ -16,6 +22,7 @@ void loop() {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   
+  //listening for wireless sensors
   if (vw_get_message(buf, &buflen)) // Non-blocking 
   {
     Serial.print("New message in: ");
@@ -25,17 +32,28 @@ void loop() {
     }
     Serial.println(actualMessage);
     
-    if (IsMessageValid()){
-      digitalWrite(pinOutputLed, HIGH); 
-      delay(1000);
-      digitalWrite(pinOutputLed, LOW); 
-    }
+    if (IsValidMessage())
+      NotifySensorTriggered();
   }
+  
+  //checking wired sensors
+  if (digitalRead(pinInputSensor1) == HIGH)
+    NotifySensorTriggered();
+
 }
 
-boolean IsMessageValid(){
+boolean IsValidMessage(){
   if ((actualMessage[0] == expectedMessage[0]) && (actualMessage[1] == expectedMessage[1]) && (actualMessage[2] == expectedMessage[2]) && (actualMessage[3] == expectedMessage[3]))
     return true;
   else
     return false;
 }
+
+void NotifySensorTriggered(){
+  digitalWrite(pinOutputLed, HIGH); 
+  digitalWrite(pinOutputSensorTriggered, HIGH); 
+  delay(2000);
+  digitalWrite(pinOutputLed, LOW); 
+  digitalWrite(pinOutputSensorTriggered, LOW); 
+}
+
